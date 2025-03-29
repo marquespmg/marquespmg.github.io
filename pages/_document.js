786414f26bea68e5277b3aaecbd39f3592b5d089
marques vendas <marquesfilhoantonio777@gmail.com>
2025-03-29 19:23:53 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-        {/* Remova o <title> daqui */}
+        {/* Ícone da aba do navegador */}
         <link rel="icon" href="/logo.png" />
         <meta name="description" content="Sua loja de produtos especiais" />
+
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-89LSRYEHF1"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-89LSRYEHF1');
+          `,
+        }} />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
