284e37e6dc8ae9dd6b0bc6f334b2138abc2e11b9
marques vendas <marquesfilhoantonio777@gmail.com>
2025-03-31 07:48:43 -0300

    Update supabaseClient.js

--- pages/supabaseClient.js
+++ pages/supabaseClient.js
@@ -1,7 +1,6 @@
-// pages/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://uwomnsxllyftjvedfkup.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3b21uc3hsbHlmdGp2ZWRma3VwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4MjQ3MDcsImV4cCI6MjA1ODQwMDcwN30.uV84j80gcqr1--8b_bVUO62gM09pRdZ2yVfNMqIce5Y'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
