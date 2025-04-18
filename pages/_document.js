66accffd01c6a2aabb90cf940aad2d6a607fe6f9
marques vendas <marquesfilhoantonio777@gmail.com>
2025-04-18 19:40:36 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
+        {/* Ícone da aba do navegador */}
         <link rel="icon" href="/logo.png" />
         <meta name="description" content="Sua loja de produtos especiais" />
 
@@ -18,7 +19,7 @@ export default function Document() {
           `,
         }} />
 
-        {/* Meta Pixel Code - Versão Simplificada e Corrigida */}
+        {/* Meta Pixel Code */}
         <script dangerouslySetInnerHTML={{
           __html: `
             !function(f,b,e,v,n,t,s)
@@ -29,79 +30,15 @@ export default function Document() {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            
             fbq('init', '9491377657643670');
             fbq('track', 'PageView');
-            
-            // Função auxiliar para verificar se o pixel está carregado
-            function waitForFbq(callback) {
-              if(typeof fbq === 'function') {
-                callback();
-              } else {
-                setTimeout(function() { waitForFbq(callback); }, 100);
-              }
-            }
-            
-            // Detecção automática de ViewContent em páginas de produto
-            if(window.location.pathname.includes('/produtos')) {
-              waitForFbq(function() {
-                fbq('track', 'ViewContent', {
-                  content_name: document.title,
-                  content_ids: [window.location.pathname.split('/').pop()],
-                  content_type: 'product',
-                  currency: 'BRL'
-                });
-              });
-            }
-            
-            // Detecção de AddToCart via mutation observer
-            new MutationObserver(function(mutations) {
-              mutations.forEach(function(mutation) {
-                if (mutation.addedNodes.length) {
-                  Array.from(mutation.addedNodes).forEach(function(node) {
-                    if (node.nodeType === 1 && node.innerText && 
-                        (node.innerText.includes('Item adicionado!') || 
-                         node.innerText.includes('Adicionado ao carrinho'))) {
-                      waitForFbq(function() {
-                        fbq('track', 'AddToCart', {
-                          content_type: 'product',
-                          currency: 'BRL'
-                        });
-                      });
-                    }
-                  });
-                }
-              });
-            }).observe(document.body, {
-              childList: true,
-              subtree: true
-            });
-            
-            // Detecção de InitiateCheckout
-            if(window.location.pathname.includes('/checkout') {
-              waitForFbq(function() {
-                fbq('track', 'InitiateCheckout', {
-                  content_type: 'product',
-                  currency: 'BRL'
-                });
-              });
-            }
-            
-            // Detecção de Lead
-            if(document.body.innerText.includes('Cadastro realizado com sucesso!')) {
-              waitForFbq(function() {
-                fbq('track', 'Lead', {
-                  content_name: 'Cadastro realizado',
-                  currency: 'BRL'
-                });
-              });
-            }
-          `
+          `,
         }} />
         <noscript>
           <img height="1" width="1" style={{ display: 'none' }}
             src="https://www.facebook.com/tr?id=9491377657643670&ev=PageView&noscript=1" />
         </noscript>
+        {/* End Meta Pixel Code */}
       </Head>
       <body>
         <Main />
