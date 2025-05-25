0d1319782db1958d436bb0c04b79b1e7c9c39d47
marques vendas <marquesfilhoantonio777@gmail.com>
2025-05-25 11:37:15 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -36,19 +36,42 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Carrinho de compras profissional" />
       </Head>
 
-      {/* Scripts de Analytics (mantidos) */}
-      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-89LSRYEHF1" />
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-89LSRYEHF1"
+      />
       <Script
         id="google-analytics"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{ /* ... */ }}
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){ dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', 'G-89LSRYEHF1');
+          `,
+        }}
       />
 
-      {/* Meta Pixel (mantido) */}
+      {/* Meta Pixel do Facebook */}
       <Script
         id="facebook-pixel"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{ /* ... */ }}
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '9491377657643670');
+            fbq('track', 'PageView');
+          `
+        }}
       />
 
       {/* Componente principal + Chat Widget */}
