9a966e817bbbe608fe01c03197a7ac6195f3df08
marques vendas <marquesfilhoantonio777@gmail.com>
2025-10-14 07:03:47 -0300

    Update [id].js

--- pages/produto/[id].js
+++ pages/produto/[id].js
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import { supabase } from '../lib/supabaseClient';
-import Cart from './pages/cart';
+import Cart from '../pages/cart';
 
 
 // Array de produtos (substitua pelos seus dados reais)
@@ -2522,4 +2522,5 @@ export async function getStaticPaths() {
     fallback: 'blocking' // Gera páginas não pré-renderidas sob demanda
   };
 }
+
 
