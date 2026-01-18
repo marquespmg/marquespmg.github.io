08d440153d431247fc74aefb3dc62e1a1714d78e
marques vendas <marquesfilhoantonio777@gmail.com>
2026-01-18 20:58:56 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  async rewrites() {
+    return [
+      {
+        source: '/food-news/:slug',
+        destination: '/food-news?slug=:slug',
+      },
+      {
+        source: '/food-news/:slug/:id',
+        destination: '/food-news?slug=:slug&page=:id',
+      },
+      // Mantém compatibilidade com URLs antigas
+      {
+        source: '/food-news',
+        destination: '/food-news',
+      }
+    ];
+  },
 };
 
 export default nextConfig;
