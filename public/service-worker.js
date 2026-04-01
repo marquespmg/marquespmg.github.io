64b5e01ab5e633a1818887dd8b5559869eabe39f
marques vendas <marquesfilhoantonio777@gmail.com>
2026-04-01 17:34:42 -0300

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,74 @@
+// service-worker.js
+const CACHE_NAME = 'pmg-cache-v1';
+const urlsToCache = [
+  '/',
+  '/produtos',
+  '/quem-somos',
+  '/politica-de-privacidade',
+  '/termos',
+  '/faq',
+  '/logo.png',
+  '/food-news',
+  'https://i.imgur.com/pBH5WpZ.png',
+  'https://i.imgur.com/jrERRsC.png'
+];
+
+// Instala o Service Worker e cacheia os arquivos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Intercepta as requisições e serve do cache quando offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - retorna a resposta do cache
+        if (response) {
+          return response;
+        }
+        
+        // Clone da requisição
+        const fetchRequest = event.request.clone();
+        
+        return fetch(fetchRequest).then(response => {
+          // Verifica se a resposta é válida
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          
+          // Clone da resposta
+          const responseToCache = response.clone();
+          
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          
+          return response;
+        });
+      })
+  );
+});
+
+// Atualiza o cache quando o service worker é ativado
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
