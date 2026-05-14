d0f09f7d542ca4473df7c56b3eeb17f0181354fd
marques vendas <marquesfilhoantonio777@gmail.com>
2026-05-14 17:40:39 -0300

    Update ofertas.js

--- pages/ofertas.js
+++ pages/ofertas.js
@@ -169,6 +169,13 @@ const featuredProducts = [
   { id: 1918, name: 'REFRESCO TANGERINA QUALIMAX 1 KILO (CX 10 PCT)', category: 'Ofertas', price: 115.00, image: 'https://www.marquesvendaspmg.shop/images/refresco-tangerina-qualimax-1-kilo.png' },
   { id: 2303, name: 'AMIDO DE MILHO QUALIMAX 1 KILO', category: 'Ofertas', price: 15.20, image: 'https://www.marquesvendaspmg.shop/images/amido-de-milho-qualimax.png' },
   { id: 2306, name: 'CALDO PARA FRUTOS DO MAR QUALIMAX 500 G', category: 'Ofertas', price: 12.70, image: 'https://www.marquesvendaspmg.shop/images/caldo-para-frutos-do-mar-qualimax.png' },
+  { id: 507, name: 'PEITO DE FRANGO CONGELADO COZIDO DESFIADO ALFAMA 1 KILO (CX 6 PCT)', category: 'Ofertas', price: 189.00, image: 'https://www.marquesvendaspmg.shop/images/peito-de-frango-congelado-cozido-desfiado-alfama-1-kilo-cx-6-pct-pmg-atacadista.jpg' },
+  { id: 527, name: 'CARNE DE SOL BOVINA CONGELADA DESFIADA ALFAMA 1 KG', category: 'Ofertas', price: 58.69, image: 'https://www.marquesvendaspmg.shop/images/carne-de-sol-bovina-congelada-desfiada-alfama-1-kg-pmg-atacadista.jpg' },
+  { id: 529, name: 'CARNE MOÍDA BOVINA CONGELADA ALFAMA 1 KILO', category: 'Ofertas', price: 28.14, image: 'https://www.marquesvendaspmg.shop/images/carne-moida-bovina-congelada-alfama-1-kg-pmg-atacadista.jpg' },
+  { id: 533, name: 'CARNE SECA BOVINA CONGELADA DESFIADA ALFAMA 1 KILO', category: 'Ofertas', price: 63.90, image: 'https://www.marquesvendaspmg.shop/images/carne-seca-bovina-congelada-desfiada-alfama-1-kg-pmg-atacadista.jpg' },
+  { id: 546, name: 'COSTELA BOVINA CONGELADA DESFIADA ALFAMA 1 KILO', category: 'Ofertas', price: 63.90, image: 'https://www.marquesvendaspmg.shop/images/costela-bovina-congelada-desfiada-alfama-1-kg-pmg-atacadista.jpg' },
+  { id: 556, name: 'CUPIM BOVINO CONGELADO DESFIADO ALFAMA 1 KILO', category: 'Ofertas', price: 63.90, image: 'https://www.marquesvendaspmg.shop/images/cupim-bovino-congelado-desfiado-alfama-1-kg-pmg-atacadista.jpg' },
+  { id: 1052, name: 'PERNIL SUÍNO CONGELADO DESFIADO ALFAMA 1 KILO PCT', category: 'Ofertas', price: 35.70, image: 'https://www.marquesvendaspmg.shop/images/pernil-suino-congelado-desfiado-alfama-1-kilo-pct-pmg-atacadista.jpg' },
 ];
 
 // ========== BANNERS ========== //
