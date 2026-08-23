94aec94064c18ce1811180a1df0aba9313beb2de
marques vendas <marquesfilhoantonio777@gmail.com>
2026-08-23 17:10:05 -0300

    Update ofertas.js

--- constants/ofertas.js
+++ constants/ofertas.js
@@ -1,15 +1,15 @@
 // constants/ofertas.js
 export const featuredProducts = [
-  { id: 2851, name: 'CARNE SECA BOVINA CONGELADA DESFIADA QUALI 1 KILO', category: 'Ofertas', price: 62.99, image: 'https://www.marquesvendaspmg.shop/images/carne-seca-bovina-congelada-desfiada-quali-1-kg-cx-6-pct.webp' },
-  { id: 2856, name: 'COSTELA BOVINA CONGELADA DESFIADA QUALI 1 KILO', category: 'Ofertas', price: 60.89, image: 'https://www.marquesvendaspmg.shop/images/costela-bovina-congelada-desfiada-quali-1-kg-cx-6-pct.webp' },
-  { id: 2861, name: 'CUPIM BOVINO CONGELADO DESFIADO QUALI 1 KILO', category: 'Ofertas', price: 65.99, image: 'https://www.marquesvendaspmg.shop/images/cupim-bovino-congelado-desfiado-quali-1-kg-cx-6-pct.webp' },
-  { id: 2878, name: 'PEITO DE FRANGO CONGELADO COZIDO DESFIADO QUALI 1 KILO (CX 15 PCT)', category: 'Ofertas', price: 434.89, image: 'https://www.marquesvendaspmg.shop/images/peito-de-frango-congelado-cozido-desfiado-quali-1-kg-cx-15-pct.webp' },
-  { id: 2879, name: 'PERNIL SUÍNO CONGELADO DESFIADO QUALI 1 KILO', category: 'Ofertas', price: 35.20, image: 'https://www.marquesvendaspmg.shop/images/pernil-suino-congelado-desfiado-quali-1-kg-cx-6-pct.webp' },
-  { id: 2875, name: 'PASTA DE ALHO SANTA MASSA 1,010 KILO', category: 'Ofertas', price: 24.80, image: 'https://www.marquesvendaspmg.shop/images/pasta-de-alho-santa-massa-1010-kg-cx-5-bis.png' },
-  { id: 2876, name: 'PATINHO BOVINO CONGELADO EM BIFE ALFAMA 2 KILO', category: 'Ofertas', price: 105.00, image: 'https://www.marquesvendaspmg.shop/images/patinho-bovino-congelado-em-bife-alfama-2-kg-cx-3-pct.webp' },
-  { id: 2877, name: 'PATINHO BOVINO CONGELADO EM TIRAS ALFAMA 2 KILO', category: 'Ofertas', price: 99.99, image: 'https://www.marquesvendaspmg.shop/images/patinho-bovino-congelado-em-tiras-alfama-2-kg-cx-3-pct.webp' },
-  { id: 2850, name: 'BISTECA DO CONTRA FILÉ BOVINO COM OSSO CONGELADA EM BIFE ALFAMA 2 KILO', category: 'Ofertas', price: 84.80, image: 'https://www.marquesvendaspmg.shop/images/bisteca-do-contra-file-bovino-com-osso-congelada-em-bife-alfama-2-kg-cx-3-pct.webp' },
-  { id: 2873, name: 'MORTADELA DEFUMADA SADIA 4 KG', category: 'Ofertas', price: 18.25, image: 'https://www.marquesvendaspmg.shop/images/mortadela-defumada-sadia-4-kg-cx-2-pc.webp' },
+  { id: 2895, name: 'VINHO ITALIANO BRANCO SUAVE FRISANTE LAMBRUSCO SOGNO ITALIANO 750 ML', category: 'Ofertas', price: 24.30, image: 'https://www.marquesvendaspmg.shop/images/vinho-italiano-branco-suave-frisante-lambrusco-sogno-italiano-750-ml-cx-6-un.png' },
+  { id: 2896, name: 'VINHO ITALIANO TINTO SUAVE FRISANTE LAMBRUSCO SOGNO ITALIANO 750 ML', category: 'Ofertas', price: 24.30, image: 'https://www.marquesvendaspmg.shop/images/vinho-italiano-tinto-suave-frisante-lambrusco-sogno-italiano-750-ml-cx-6-un.png' },
+  { id: 2883, name: 'AZEITONA PRETA GRAÚDA 11 X 13 AZAPA ARCOBELLO (BD 2 KILO)', category: 'Ofertas', price: 114.80, image: 'https://www.marquesvendaspmg.shop/images/azeitona-preta-grauda-11-x-13-azapa-arcobello-bd-2-kg.webp' },
+  { id: 2884, name: 'BATATA PALITO CONGELADA PRÉ FRITA 7 MM CORTE FINO FAST FOOD BEM BRASIL 2 KILO (CX 6 PCT)', category: 'Ofertas', price: 175.99, image: 'https://www.marquesvendaspmg.shop/images/batata-palito-congelada-pre-frita-7-mm-corte-fino-fast-food-bem-brasil-2-kg-cx-6-pct.jpg' },
+  { id: 2853, name: 'CHEDDAR FORTUNATA CREMOSO RECHEIO 1,010 KILO', category: 'Ofertas', price: 17.99, image: 'https://www.marquesvendaspmg.shop/images/cheddar-fortunata-cremoso-recheio-1010-kg-cx-8-bis.png' },
+  { id: 2882, name: 'REQUEIJÃO FORTUNATA CREMOSO RECHEIO 1,010 KILO', category: 'Ofertas', price: 17.99, image: 'https://www.marquesvendaspmg.shop/images/requeijao-fortunata-cremoso-recheio-1010-kg-cx-8-bis.webp' },
+  { id: 1908, name: 'CHÁ MATTE LEÃO NATURAL TOSTADO A GRANEL 250 G (CX 10 UN)', category: 'Ofertas', price: 74.30, image: 'https://www.marquesvendaspmg.shop/images/cha-matte-leao-natural-tostado-a-granel-250-g.png' },
+  { id: 1909, name: 'CHÁ MATTE LEÃO SOLÚVEL NATURAL 1 KILO', category: 'Ofertas', price: 33.60, image: 'https://www.marquesvendaspmg.shop/images/cha-matte-leao-soluvel-natural-1-kilo.png' },
+  { id: 2849, name: 'BATATA PALITO DOCE CONGELADA PRÉ FRITA 9 MM CORTE TRADICIONAL FARM FRITES 2 KILO (CX 5 PCT)', category: 'Ofertas', price: 378.00, image: 'https://www.marquesvendaspmg.shop/images/batata-palito-doce-congelada-pre-frita-9-mm-corte-tradicional-farm-frites-2-kg-cx-5-pct.png' },
+  { id: 319, name: 'VINHO NACIONAL TINTO SUAVE COUNTRY WINE 750 ML (CX 6 UN)', category: 'Ofertas', price: 85.00, image: 'https://www.marquesvendaspmg.shop/images/vinho-nacional-tinto-suave-country-wine-750-ml-pmg-atacadista.jpg' },
 ];
 
 // Array com os IDs em oferta (fácil de verificar)
