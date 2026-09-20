ffbd3f0aa1d43877cd28a681d051f20a51286326
marques vendas <marquesfilhoantonio777@gmail.com>
2026-09-20 15:58:49 -0300

    Update ofertas.js

--- pages/ofertas.js
+++ pages/ofertas.js
@@ -140,22 +140,16 @@ sp: {
 
 // ========== PRODUTOS EM OFERTA ========== //
 const featuredProducts = [
-  { id: 2853, name: 'CHEDDAR FORTUNATA CREMOSO RECHEIO 1,010 KILO', category: 'Ofertas', price: 13.93, image: 'https://www.marquesvendaspmg.shop/images/cheddar-fortunata-cremoso-recheio-1010-kg-cx-8-bis.png' },
-  { id: 2882, name: 'REQUEIJÃO FORTUNATA CREMOSO RECHEIO 1,010 KILO', category: 'Ofertas', price: 13.62, image: 'https://www.marquesvendaspmg.shop/images/requeijao-fortunata-cremoso-recheio-1010-kg-cx-8-bis.webp' },
-  { id: 1222, name: 'CAFÉ EXTRA FORTE ALMOFADA SELETO 500 G (FDO 10 PCT)', category: 'Ofertas', price: 197.44, image: 'https://www.marquesvendaspmg.shop/images/cafe-extra-forte-almofada-seleto-500-g-fdo-10-pct-pmg-atacadista.jpg' },
-  { id: 1229, name: 'CAFÉ TRADICIONAL ALMOFADA SELETO 500 G (FDO 10 PCT)', category: 'Ofertas', price: 197.44, image: 'https://www.marquesvendaspmg.shop/images/cafe-tradicional-almofada-seleto-500-g-fdo-10-pct-pmg-atacadista.jpg' },
-  { id: 1224, name: 'CAFÉ EXTRA FORTE ALMOFADA UNIÃO 500 G (CX 10 PCT)', category: 'Ofertas', price: 233.03, image: 'https://www.marquesvendaspmg.shop/images/cafe-extra-forte-almofada-uniao-500-g-cx-10-pct-pmg-atacadista.jpg' },
-  { id: 1230, name: 'CAFÉ TRADICIONAL ALMOFADA UNIÃO 500 G (CX 10 PCT)', category: 'Ofertas', price: 233.03, image: 'https://www.marquesvendaspmg.shop/images/cafe-tradicional-almofada-uniao-500-g-cx-10-pct-pmg-atacadista.jpg' },
-  { id: 1121, name: 'AZEITE DE OLIVA EXTRA VIRGEM COCINERO 500 ML', category: 'Ofertas', price: 23.15, image: 'https://www.marquesvendaspmg.shop/images/azeite-de-oliva-extra-virgem-cocinero-500-ml-pmg-atacadista.jpg' },
-  { id: 1446, name: 'ATUM PEQUENO RALADO EM ÓLEO PESCADOR 140 G (CX 24 LT)', category: 'Ofertas', price: 170.53, image: 'https://www.marquesvendaspmg.shop/images/atum-pequeno-ralado-em-oleo-pescador-140-g-cx-24-lt-pmg-atacadista.jpg' },
-  { id: 1450, name: 'ATUM PEQUENO SÓLIDO EM ÓLEO PESCADOR 140 G (CX 24 LT)', category: 'Ofertas', price: 221.47, image: 'https://www.marquesvendaspmg.shop/images/atum-pequeno-solido-em-oleo-pescador-140-g-cx-24-lt-pmg-atacadista.jpg' },
-  { id: 2234, name: 'SARDINHAS EM ÓLEO PESCADOR 125 G (CX 54 LT)', category: 'Ofertas', price: 289.95, image: 'https://www.marquesvendaspmg.shop/images/sardinhas-em-oleo-pescador.png' },
-  { id: 1439, name: 'ATUM PEDAÇOS EM ÓLEO POUCH COQUEIRO 480 G', category: 'Ofertas', price: 29.31, image: 'https://www.marquesvendaspmg.shop/images/atum-pedacos-em-oleo-pouch-coqueiro-480-g-pmg-atacadista.jpg' },
-  { id: 1444, name: 'ATUM PEQUENO RALADO EM ÓLEO COQUEIRO 170 G (CX 24 LT)', category: 'Ofertas', price: 200.32, image: 'https://www.marquesvendaspmg.shop/images/atum-pequeno-ralado-em-oleo-coqueiro-170-g-cx-24-lt-pmg-atacadista.jpg' },
-  { id: 1448, name: 'ATUM PEQUENO SÓLIDO EM ÓLEO COQUEIRO 170 G (CX 24 LT)', category: 'Ofertas', price: 265.03, image: 'https://www.marquesvendaspmg.shop/images/atum-pequeno-solido-em-oleo-coqueiro-170-g-cx-24-lt-pmg-atacadista.jpg' },
-  { id: 1468, name: 'SARDINHAS EM ÓLEO COQUEIRO 125 G (CX 54 LT)', category: 'Ofertas', price: 321.84, image: 'https://www.marquesvendaspmg.shop/images/sardinhas-em-oleo-coqueiro-125-g-cx-54-lt-pmg-atacadista.jpg' },
-  { id: 2587, name: 'MACARRÃO CARACOLINO COM OVOS SANTA AMÁLIA 500 G (FDO 20 PCT)', category: 'Ofertas', price: 72.04, image: 'https://www.marquesvendaspmg.shop/images/macarrao-caracolino-com-ovos-santa-amalia-500-g-fdo-20-pct.webp' },
-  { id: 2591, name: 'MACARRÃO NINHO TALHARINE COM OVOS SANTA AMÁLIA 500 G (FDO 20 PCT)', category: 'Ofertas', price: 97.81, image: 'https://www.marquesvendaspmg.shop/images/macarrao-ninho-talharine-com-ovos-santa-amalia-500-g-fdo-20-pct.webp' },
+  { id: 1222, name: 'CAFÉ EXTRA FORTE ALMOFADA SELETO 500 G (FDO 10 PCT)', category: 'Ofertas', price: 196.00, image: 'https://www.marquesvendaspmg.shop/images/cafe-extra-forte-almofada-seleto-500-g-fdo-10-pct-pmg-atacadista.jpg', gtin: '7896053600013' },
+  { id: 1229, name: 'CAFÉ TRADICIONAL ALMOFADA SELETO 500 G (FDO 10 PCT)', category: 'Ofertas', price: 196.00, image: 'https://www.marquesvendaspmg.shop/images/cafe-tradicional-almofada-seleto-500-g-fdo-10-pct-pmg-atacadista.jpg', gtin: '7896053600044' },
+  { id: 1121, name: 'AZEITE DE OLIVA EXTRA VIRGEM COCINERO 500 ML', category: 'Ofertas', price: 23.15, image: 'https://www.marquesvendaspmg.shop/images/azeite-de-oliva-extra-virgem-cocinero-500-ml-pmg-atacadista.jpg', gtin: '7790070218216' },
+  { id: 1439, name: 'ATUM PEDAÇOS EM ÓLEO POUCH COQUEIRO 480 G', category: 'Ofertas', price: 29.31, image: 'https://www.marquesvendaspmg.shop/images/atum-pedacos-em-oleo-pouch-coqueiro-480-g-pmg-atacadista.jpg', gtin: '7896009301209' },
+  { id: 1454, name: 'ATUM RALADO EM ÓLEO POUCH COQUEIRO 480 G', category: 'Ofertas', price: 22.90, image: 'https://www.marquesvendaspmg.shop/images/atum-ralado-em-oleo-pouch-coqueiro-480-g-pmg-atacadista.jpg', gtin: '7896009301216' },
+  { id: 2970, name: 'BATATA PALITO CONGELADA PRÉ FRITA 10 MM CORTE TRADICIONAL SIMPLOT 2,5 KILO (CX 6 PCT)', category: 'Ofertas', price: 176.05, image: 'https://www.marquesvendaspmg.shop/images/batata-palito-congelada-pre-frita-10-mm-corte-tradicional-simplot-25-kg-cx-6-pct.webp', gtin: '5412588061079' },
+  { id: 2971, name: 'CARNE MOÍDA BOVINA E VEGETAL CONGELADA SEARA 1 KILO', category: 'Ofertas', price: 24.91, image: 'https://www.marquesvendaspmg.shop/images/carne-moida-bovina-e-vegetal-congelada-seara-1-kg-cx-5-pct.png', gtin: '7894904608225' },
+  { id: 2975, name: 'FARINHA DE TRIGO PIZZERIA TIPO 1 ANACONDA 5 KILO (FDO 25 KILO)', category: 'Ofertas', price: 312.65, image: 'https://www.marquesvendaspmg.shop/images/farinha-de-trigo-pizzeria-tipo-1-anaconda-5-kg-fdo-25-kg.png', gtin: '7896419471196' },
+  { id: 2980, name: 'MOLHO AMERICANO CHICKEN ZAFRÁN 1,1 KILO', category: 'Ofertas', price: 37.64, image: 'https://www.marquesvendaspmg.shop/images/molho-americano-chicken-zafran-11-kg-cx-05-bag.png', gtin: '7898615177087' },
+  { id: 2979, name: 'FRANGO A PASSARINHO CONGELADO TEMPERADO ADORO 1 KILO (CX 20 PCT)', category: 'Ofertas', price: 183.15, image: 'https://www.marquesvendaspmg.shop/images/frango-a-passarinho-congelado-temperado-adoro-1-kg-cx-20-pct.webp', gtin: '7897560912491' },
 ];
 
 // ========== BANNERS ========== //
